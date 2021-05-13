861859b182c27c0a9034715519613e903f9f8c8d
Ante Filipović <Ante.filipovic72@gmail.com>
2021-05-13 22:34:58 +0200

    Adding tables

--- Vjezba.sql
+++ Vjezba.sql
@@ -1,3 +1,37 @@
 DROP database IF EXISTS vjezba;
 CREATE database vjezba;
 use vjezba;
+
+
+CREATE TABLE zupanija (
+
+	sifra int auto_increment PRIMARY KEY,
+	naziv varchar(50),
+	zupan int
+
+);
+
+
+CREATE TABLE zupan (
+
+	sifra int auto_increment PRIMARY KEY,
+	ime varchar(50),
+	prezime varchar(50)
+
+);
+
+CREATE TABLE opcina (
+
+	sifra int auto_increment PRIMARY KEY,
+	zupanija int,
+	naziv varchar(50)
+
+);
+
+CREATE TABLE mjesto (
+
+	sifra int auto_increment PRIMARY KEY,
+	opcina int,
+	naziv varchar(50)
+
+);
