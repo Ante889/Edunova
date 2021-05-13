5d99d6db07ad6044d5bf41a530b8f79eae085480
Ante Filipović <Ante.filipovic72@gmail.com>
2021-05-13 22:47:08 +0200

    adding foreign keys

--- Vjezba.sql
+++ Vjezba.sql
@@ -35,3 +35,8 @@ CREATE TABLE mjesto (
 	naziv varchar(50)
 
 );
+
+
+alter table zupanija add foreign key (zupan) references zupan(sifra); 
+alter table opcina add foreign key (zupanija) references zupanija(sifra); 
+alter table mjesto add foreign key (opcina) references opcina(sifra); 
