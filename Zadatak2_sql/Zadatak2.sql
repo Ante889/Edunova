107db8c0ccd59e299c51e275c67d730437538e00
Ante Filipović <Ante.filipovic72@gmail.com>
2021-05-21 13:03:30 +0200

    Zadatak 3 riješenje

--- Zadatak2_sql/Zadatak2.sql
+++ Zadatak2_sql/Zadatak2.sql
@@ -1,3 +1,24 @@
+#Zadaci za kolokvij_vjezba_2 (u zagradi je maksimalan broj bodova)
+
+#0. Kreirajte tablice (16) i veze izmeðu tablica. (6)
+
+#1. U tablice neprijatelj, cura i decko_zarucnica unesite po 3 retka. (7)
+
+#2. U tablici prijatelj postavite svim zapisima kolonu treciputa na
+#vrijednost 30. travnja 2020. (4)
+
+#3. U tablici brat obrišite sve zapise èija je vrijednost kolone ogrlica
+#razlièito od 14. (4)
+
+#4. Izlistajte suknja iz tablice cura uz uvjet da vrijednost kolone
+#drugiputa nepoznate. (6)
+
+#5. Prikažite novcica iz tablice zarucnica, neprijatelj iz tablice brat te
+#haljina iz tablice neprijatelj uz uvjet da su vrijednosti kolone
+#drugiputa iz tablice cura poznate te da su vrijednosti kolone vesta iz
+#tablice decko sadrže niz znakova ba. Podatke posložite po haljina iz
+#tablice neprijatelj silazno. (10)
+
 DROP database IF EXISTS zadatak2;
 CREATE database zadatak2;
 use zadatak2;
