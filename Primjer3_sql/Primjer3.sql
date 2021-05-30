fdded9eea70205ee0f68aef84e1f52e7cea681fa
Ante Filipović <Ante.filipovic72@gmail.com>
2021-05-30 23:38:05 +0200

    Riješena prva 3 zadatka

--- Primjer3_sql/Primjer3.sql
+++ Primjer3_sql/Primjer3.sql
@@ -1,3 +1,5 @@
+#0. Kreirajte tablice (10) i veze izmeğu tablica. (3)
+
 DROP database IF EXISTS Primjer3;
 CREATE database Primjer3;
 use Primjer3;
@@ -18,7 +20,7 @@ create table prijateljica (
 	id int not null primary key auto_increment,
 	gustoca decimal(13,6),
 	maraka decimal(14,9) not null,
-	treciputa datetime not null,
+	treciputa datetime,
 	narukvica int not null
 	
 );
@@ -57,3 +59,57 @@ create table punica (
 );
 
 alter table neprijatelj add foreign key (prijateljica) references prijateljica(id);
+
+
+#1. Kreirati funkciju zadatak1 koja osigurava kako cjelobrojni tipovi podataka moraju biti izmeğu 1346 i 5367
+#(7). Primjeniti funkciju u minimalno jednom upitu u proceduri ili okidaèu (7).
+
+delimiter $$
+create function Zadatak1(A int) returns int begin
+	if (a > 1346 and a < 5367) then 
+	return a;
+	else 
+	return null;
+	end if;
+end
+$$
+delimiter ;
+
+insert into prijateljica (maraka, narukvica) values (1.1, Zadatak1(4000));
+### ERROR --- insert into prijateljica (maraka, narukvica) values (1.1, Zadatak1(2)); --- ERROR ###
+select * from prijateljica;
+
+
+#2. Kreirajte proceduru zadatak2 koja unosi 33501 zapisa u tablicu ostavljen (7). Izvesti proceduru jednom
+#tako da u tablici bude toèno 33501 zapisa (7).
+
+delimiter $$
+create procedure Zadatak2() 
+begin 
+	declare A int default 0;
+	while (A < 33501) do 
+	insert into ostavljen(id) values (0);
+	set A=A+1;
+	end while;
+end;
+$$
+delimiter ;
+
+select count(*) from ostavljen;
+delete from ostavljen; 
+call zadatak2;
+
+
+#3. Kreirajte okidaè zadatak3 nakon insert-a u tablicu ostavljen tako da za svaki unos u tablicu ostavljen
+#napravi po dva unosa u tablicu djevojka (7). Okidaè je u funkciji, tablica djevojka ima dvostruko više zapisa
+#od tablice ostavljen (7).
+
+delimiter $$ 
+create trigger Zadatak3 after insert on ostavljen for each row 
+begin 
+	insert into djevojka (majica, indiferentno, narukvica) values ("",1,1),("",1,1);
+end
+$$
+delimiter ;
+
+select count(*) from djevojka;
