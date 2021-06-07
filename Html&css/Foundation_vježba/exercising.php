6b018fac9853f304ac3190529248cf554a98ba06
Ante Filipović <Ante.filipovic72@gmail.com>
2021-06-07 23:55:12 +0200

    Test

--- Html&css/Foundation_vježba/exercising.php
+++ Html&css/Foundation_vježba/exercising.php
@@ -27,6 +27,43 @@ if(isset($_GET['color'])){
 <a href="exercising.php?color=red">RED</a>
 <a href="exercising.php?color=green">GREEN</a>
 <a href="exercising.php?color=white">WHITE</a>
+<hr><hr><hr>
+
+
+        <form action="" method="post">
+        <label for="">Name</label>
+            <input type="text" name="Name" placeholder="input name">
+        <label for="">Lastname</label>    
+            <input type="text" name="Lastname" placeholder="input lastname">
+        <label for="">Email</label>    
+            <input type="email" name="Email" placeholder="input email">    
+        <label for="">Password</label>    
+            <input type="password" name="Password" placeholder="input password">
+        <label for="">Confirm password</label>    
+            <input type="password" name="Confirm" placeholder="confirm password">    
+            <input type="submit" name="submit" class="button">
+
+        </form>
+
+
+<?php 
+
+if(isset($_POST['submit'])){
+
+    $Name=$_POST['Name'];
+    $Lastname=$_POST['Lastname'];
+    $Email=$_POST['Email'];
+    $Password=$_POST['Password'];
+    $Confirm_Password=$_POST['Confirm'];
+    echo $Name . '<br>',
+         $Lastname . '<br>',
+         $Email . '<br>',
+         $Password . '<br>',
+         $Confirm_Password . '<br>';
+
+}
+
+?>
 
 
 <hr>
