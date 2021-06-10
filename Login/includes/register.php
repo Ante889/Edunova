bd6fc0d3b829576c251118832db705f09ceb2fe8
Ante Filipović <Ante.filipovic72@gmail.com>
2021-06-10 23:09:52 +0200

    Finishing register code

--- Login/includes/register.php
+++ Login/includes/register.php
@@ -3,10 +3,26 @@
 
 class register
 {
-    function register_user($Username, $Email, $Password){
+    function register_user($Username, $Email, $Password, $Confirm_password){
 
-        
+        $Errors = array(
+            'Password' => '',
+            'Email' => '',
+            'Empty' => ''
+        );
 
+        if($Confirm_password != $Password){
+            $Errors['Password'] = "Passwords doesn't match";
+        }elseif(strlen($Password) < 6){
+            $Errors['Password'] = "Min 6 characters";
+        }elseif(!$this -> user_exists($Username, $Email)){
+            $Errors['Email'] = "Email or username exsists";
+        }elseif(empty($Username) || empty($Password) || empty($Email) || empty($Confirm_password)){
+            $Errors['Empty'] = "Cannot be empty";
+        }else{
+            $this -> insert_into_database($Username,$Email,$Password);
+        }
+        return $Errors;
     }
 
     function insert_into_database($Username, $Email, $Password){
@@ -30,9 +46,6 @@ class register
         if(Empty($Fetch)){
         return true;
         }
-
-        
-
     }
 }
 
