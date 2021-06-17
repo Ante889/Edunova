4129073e6d04af2cfdbab680bc22921accde7a5a
Ante Filipović <Ante.filipovic72@gmail.com>
2021-06-17 13:40:11 +0200

    Commit 2

--- Php_exercising/php.php
+++ Php_exercising/php.php
@@ -11,25 +11,34 @@ createArray(5,5);
 function createArray($column, $row){
 
     $result = array();
-    $sum= $column * $row;
     $current_row= 1;
     $current_column=1;
     $current_number=1;
+    $finished_rows=0;
+    $finished_column=0;
 
-    for ($i=$current_column; $i <= $column ; $i++) { 
-        $result[$current_row][$i] = $current_number++;
-        if($i == $column){
-            $current_row++;
-            $current_column=$i;
-        } 
-    }
-    for ($a= $current_row; $a <= $row; $a++) { 
-        $result[$a][$current_column]=$current_number++;
-        if($a == $row){
-            $current_row=$a;
-            $current_column--;
+
+
+        for ($i=$current_column; $i <= $column-$finished_column ; $i++) { 
+            $result[$current_row][$i] = $current_number++;
+            if($i == $column){
+                $current_row++;
+                $current_column=$i;
+                $finished_rows++;
+            } 
         }
-    }
+        for ($a= $current_row; $a <= $row-$finished_rows; $a++) { 
+            $result[$a][$current_column]=$current_number++;
+            if($a == $row){
+                $current_row=$a;
+                $current_column--;
+                $finished_column++;
+            }
+        }
+
+
+
+
 
     echo 'row ' . $current_row .'<br>';
     echo 'colum ' . $current_column .'<br>';
