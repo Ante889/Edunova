407a531933b2be67fbd76e9bf6c75cac2e33b186
Ante Filipović <Ante.filipovic72@gmail.com>
2021-06-18 03:17:18 +0200

    showing numbers

--- Php_exercising/php.php
+++ Php_exercising/php.php
@@ -1,13 +1,5 @@
 <?php
 
-
-if(isset($_POST['submit'])){
-
-  $brojRedaka = $_POST['BrojRedaka'];
-  $brojStupaca = $_POST['BrojStupaca'];
-}
-
-createArray(4,4);
 function createArray($column, $row){
 
     $result = array();
@@ -15,59 +7,65 @@ function createArray($column, $row){
     $current_column=1;
     $current_number=1;
 
-    for ($z=0; $z < $row ; $z++) { 
-        for ($i=$current_column; $i <= $column; $i++) { 
-            if(isset($result[$current_row][$i])){
-                continue;
-            }else{
-            $result[$current_row][$i] = $current_number++;
-            if($i == $column - $z){
-                $current_column=$i;
-            } 
+            for ($z=0; $z < $row ; $z++) { 
+                for ($i=$current_column; $i <= $column; $i++) { 
+                    if(isset($result[$current_row][$i])){
+                        continue;
+                    }else{
+                    $result[$current_row][$i] = $current_number++;
+                    if($i == $column - $z){
+                        $current_column=$i;
+                    } 
+                    }
+                    
+                }
+                for ($a= $current_row; $a <= $row; $a++) { 
+                    if(isset($result[$a][$current_column])){
+                        continue;
+                    }else{
+                    $result[$a][$current_column]=$current_number++;
+                    if($a == $row - $z){
+                        $current_row=$a;
+                    }
+                }
             }
-            
-        }
-        for ($a= $current_row; $a <= $row; $a++) { 
-            if(isset($result[$a][$current_column])){
-                continue;
-            }else{
-            $result[$a][$current_column]=$current_number++;
-            if($a == $row - $z){
-                $current_row=$a;
+                for ($b=$current_column; $b > 0; $b--) { 
+                    if(isset($result[$current_row][$b])){
+                        continue;
+                    }else{
+                    $result[$current_row][$b] = $current_number++;
+                    if($b == 1 + $z){
+                        $current_column=$b;
+                    } 
+                }
+            }
+                for ($c= $current_row; $c >= 1; $c--) { 
+                    if(isset($result[$c][$current_column])){
+                        continue;
+                    }else{
+                    $result[$c][$current_column]=$current_number++;
+                    if($c == 2 +$z){
+                        $current_row=$c;
+                    }
+                }
             }
         }
-    }
-        for ($b=$current_column; $b > 0; $b--) { 
-            if(isset($result[$current_row][$b])){
-                continue;
-            }else{
-            $result[$current_row][$b] = $current_number++;
-            if($b == 1 + $z){
-                $current_column=$b;
-            } 
-        }
-    }
-        for ($c= $current_row; $c >= 1; $c--) { 
-            if(isset($result[$c][$current_column])){
-                continue;
-            }else{
-            $result[$c][$current_column]=$current_number++;
-            if($c == 2 +$z){
-                $current_row=$c;
-            }
-        }
-    }
- }
         
-         
-        
+        foreach($result as $row => $value){
+            $row_value_sum= count($result[$row]); 
+            $row_sum =count($result); 
 
-    echo 'row ' . $current_row .'<br>';
-    echo 'colum ' . $current_column .'<br>';
+            if($row == $row_sum){
 
-    print_r($result);
+                for ($s=$row_sum; $s >= 1 ; $s--) { 
 
-
+                    for ($j=$row_value_sum; $j >=1; $j--) { 
+                        echo "<div class='square'>".$result[$s][$j]."</div>";
+                        if($j == 1){
+                            echo '<br>';
+                    }
+                }
+            }  
+        }
+    }
 }
-
-?>
