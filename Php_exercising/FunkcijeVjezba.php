dd3f5c7da37748b0723d0cc2d34ec6cf3f13bf57
Ante Filipović <Ante.filipovic72@gmail.com>
2021-06-23 23:57:05 +0200

    Funkcije vjezba

--- Php_exercising/FunkcijeVjezba.php
+++ Php_exercising/FunkcijeVjezba.php
@@ -33,3 +33,25 @@ function PrikaziProsteBrojeve($broj1,$broj2)
 }
 
 PrikaziProsteBrojeve(1,100);
+ echo '<hr>';
+
+//////////FUNKCIJE//////////////
+
+$Array= array(1,2,1,23,684,9498,1961,898,8948896,[1,2,3]);
+
+// 1. COUNT
+echo '<hr>';
+echo count($Array);
+echo count($Array, COUNT_RECURSIVE);
+
+
+// 2. is_array
+echo '<hr>';
+$String="String";
+echo is_array($Array)? 'yes':'no';
+echo '<br>';
+echo is_array($String)? 'yes':'no';
+
+
+
+
