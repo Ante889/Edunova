3f3b076e58447339958374229ee223e62cba2bef
Ante Filipović <Ante.filipovic72@gmail.com>
2021-06-24 18:36:53 +0200

    VježbaFunkcije

--- Php_exercising/FunkcijeVjezba.php
+++ Php_exercising/FunkcijeVjezba.php
@@ -35,9 +35,9 @@ function PrikaziProsteBrojeve($broj1,$broj2)
 PrikaziProsteBrojeve(1,100);
  echo '<hr>';
 
-//////////FUNKCIJE//////////////
+//////////FUNKCIJE//////////////  https://www.exakat.io/en/top-100-php-functions/
 
-$Array= array(1,2,1,23,684,9498,1961,898,8948896,[1,2,3]);
+$Array= array(1,2,1,23,684,9498,1961,898,8948896,[1,2,3],'test',1.10);
 
 // 1. COUNT
 echo '<hr>';
@@ -54,4 +54,24 @@ echo is_array($String)? 'yes':'no';
 
 
 
+// 3. substr
+echo '<hr>';
+$Test= "OvoJeTest";
+echo substr($Test, 0,3);
+echo substr($Test,3,2);
+echo substr($Test, 5);
 
+
+// 4. in_array 
+echo '<hr>';
+if(in_array(1.10,$Array, true)){
+    echo true;
+}
+
+
+//5. explode
+echo '<hr>';
+$a='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s when an unknown printer took a galley';
+print_r(explode(' ',$a));
+
+print_r(explode(' ',$a,4));
