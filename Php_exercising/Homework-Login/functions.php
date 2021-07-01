50164a89b0c8e96839974f87d7352545e68b110e
Ante Filipović <Ante.filipovic72@gmail.com>
2021-07-01 22:41:49 +0200

    Creating registration

--- Php_exercising/Homework-Login/functions.php
+++ Php_exercising/Homework-Login/functions.php
@@ -1,5 +1,5 @@
 <?php
-Database();
+$Database = Database();
 
 function Database(){
 
@@ -15,7 +15,50 @@ function Database(){
     }else{
         return $conn;
     }
+}
+
+function checkRegistration($Email,$Password){
+    
+    global $Database;
+    $Email = Escape_string($Email);
+    $Password = Escape_string($Email);
+
+    $sql = "SELECT * FROM users WHERE email = '$Email'";
+    $result=mysqli_fetch_assoc(mysqli_query($Database, $sql));
+    if(empty($result)){
+        $sql= "INSERT INTO users (email,password) values ('$Email','$Password')";
+        mysqli_query($Database, $sql);
+        return true;
+    }else{
+        return false;
+    }       
+}
+
+function RegistrationErrors($Email,$Password,$Confirm_password){
+
+    if(empty($Email) || empty($Password) || empty($Confirm_password)){
+        return "Fields must not be empty";
+    }elseif($Password !== $Confirm_password){
+        return "Passwords are not the same";
+    }elseif(strlen($Password) < 6){
+        return "Password must have a minimum of 6 characters";
+    }elseif(!checkRegistration($Email,$Password)){
+        return "Email exists";
+    }
+    header("Location: index.php");
+}
+
+function Escape_string($String){
+
+    global $Database;
+    $Result=mysqli_real_escape_string($Database,$String);
+    return $Result;
 
 }
+
+function Msg($Msg){
+
+}
+
 
 ?>
--- Php_exercising/Homework-Login/register.php
+++ Php_exercising/Homework-Login/register.php
@@ -1,34 +1,34 @@
-<?php 
+<?php include "header.php"; ?>
 
-include "header.php";
+<?php
 
+if(isset($_POST['submit-register'])){
+  $Msg=RegistrationErrors($_POST['Register-Email'],$_POST['Register-Password'],$_POST['Register-Confirm-Password']);
+}     
 ?>
 
+
 <div class="translucent-form-overlay text-center centered">
-  <form>
-    <h3>Create acount</h3>
+  <form action="" method="POST">
+    <h3>Create account</h3>
     <hr>
     <br>
     <div class="row columns text-center">
-        <input type="text" name="Email" placeholder="Email">
+        <input type="text" name="Register-Email" placeholder="Email">
     </div>
     <div class="row columns text-center">
-        <input type="Password" name="Password" placeholder="Password">
+        <input type="Password" name="Register-Password" placeholder="Password">
     </div>
     <div class="row columns text-center">
-        <input type="Password" name="Password" placeholder="Confirm password">
+        <input type="Password" name="Register-Confirm-Password" placeholder="Confirm password">
     </div>
+    <h5><?php if(isset($Msg)){echo $Msg;}?> </h5>
     <br>
-    <button type="button" class="primary button expanded search-button">
-      Register
-    </button>
-    <a href="index.php">Register</a>
+    <input type="submit" name="submit-register" class="primary button expanded search-button" value="Register">
+
+    <a href="index.php">Login</a>
  </form>
 </div>
-
-
-
-
 
 <?php 
 include "footer.php";
