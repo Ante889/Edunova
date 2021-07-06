a07bd4c01c7a5589ff9049ef6d8850c6aa0f3389
Ante Filipović <Ante.filipovic72@gmail.com>
2021-07-06 19:48:58 +0200

    fixing bugs

--- Php_exercising/Love_test/functions.php
+++ Php_exercising/Love_test/functions.php
@@ -6,15 +6,16 @@ if(isset($_POST['submit'])){
     $Names = strtolower($_POST['FirstName'] . $_POST['SecondName']);
     $Names = str_replace(' ','',$Names);
     $LettersNumber= strlen($Names);
+    for ($i=0; $i < $LettersNumber; $i++) { 
+        $Letter = substr($Names, $i,1);
+        $Values[]=substr_count($Names,$Letter);
+    }
+    echo $Value=implode('',$Values);
+    echo '<hr>';
+    Calculate($Value);
 
 }
-for ($i=0; $i < $LettersNumber; $i++) { 
-    $Letter = substr($Names, $i,1);
-    $Values[]=substr_count($Names,$Letter);
-}
-echo $Value=implode('',$Values);
-echo '<hr>';
-Calculate($Value);
+
 
 
 
@@ -23,12 +24,16 @@ function Calculate($Value)
     $sum = ceil(strlen($Value)/2);
     if($sum>=2){
         $sum--;
-        for ($i=0; $i <= $sum ; $i++) { 
+        for ($i=0; $i <=$sum ; $i++) { 
             if($i != $sum){
             $Values[$i]=substr($Value, $i,1) + substr($Value, -$i-1,1);
             }
             else{
-                $Values[$i]=substr($Value, $i,1);
+                if(strlen($Value) & 1){
+                    $Values[$i]=substr($Value, $i,1);
+                }else{
+                    $Values[$i]=substr($Value, $i,1) + substr($Value, -$i-1,1);
+                } 
             }
         }
         echo implode('',$Values).'<hr>';
