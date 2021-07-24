edabd1d856a6039bbdd814f9d0aef0044749b457
Ante Filipović <Ante.filipovic72@gmail.com>
2021-07-24 03:48:19 +0200

    adding register.php

---
+++ Ljetni_zadatak/main_part/Public/register.php
@@ -0,0 +1,22 @@
+<br>
+<div class="login">
+    <div class="translucent-form-overlay text-center centered">
+    <form action="" method="POST">
+        <h3>Create account</h3>
+        <br>
+        <div class="row columns text-center">
+            <input type="text" name="Register-Email" placeholder="Email">
+        </div>
+        <div class="row columns text-center">
+            <input type="Password" name="Register-Password" placeholder="Password">
+        </div>
+        <div class="row columns text-center">
+            <input type="Password" name="Register-Confirm-Password" placeholder="Confirm password">
+        </div>
+        <br>
+        <input type="submit" name="submit-register" class="primary button expanded search-button" value="Register">
+
+        <a href="?login">Login</a>
+    </form>
+    </div>
+</div>
