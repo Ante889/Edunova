da5eb7441e5e8123fee3661adcc851322de3e54a
Ante Filipović <Ante.filipovic72@gmail.com>
2021-07-24 03:48:32 +0200

    adding login.php

---
+++ Ljetni_zadatak/main_part/Public/login.php
@@ -0,0 +1,18 @@
+<br>
+<div class="login">
+<div class="translucent-form-overlay text-center centered">
+  <form action="" method="POST">
+    <h3>Login</h3>
+    <br>
+    <div class="row columns text-center">
+        <input type="text" name="Email" placeholder="Email">
+    </div>
+    <div class="row columns text-center">
+        <input type="Password" name="Password" placeholder="Password">
+    </div>
+    <br>
+    <input type="submit" name="submit-login" class="primary button expanded search-button" value="Login">
+    <h4>You do not have an account? </h4><a href="?register"><h4>Create acount</h4></a>
+ </form>
+</div>
+</div>
