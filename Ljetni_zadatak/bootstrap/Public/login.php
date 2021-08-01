13f4a72568760fe18aab7d52184986fd2fafbb47
Ante Filipović <Ante.filipovic72@gmail.com>
2021-08-01 03:30:31 +0200

    finishing login and register page

--- Ljetni_zadatak/bootstrap/Public/login.php
+++ Ljetni_zadatak/bootstrap/Public/login.php
@@ -10,26 +10,31 @@ if(isset($_POST['submit-login'])){
 ?>
 
 <br>
-<div class="login">
-<div class="translucent-form-overlay text-center centered">
+
   <form action="" method="POST">
-    <h3>Login</h3>
+  <div class="form-group row">
+    <div class="col-sm-5">
+    <div class="form-group">
+    <h3 class="text-center">Login</h3>
     <br>
-    <div class="row columns text-center">
-        <input type="text" name="email-login" placeholder="Email">
+    <div class="form-group text-center">
+        <input class="form-control" type="text" name="email-login" placeholder="Email">
     </div>
     <?php
         if(isset($Msg['email'])){echo "<h4 style='color:red;'>".$Msg['email']."</h4>";}
     ?>
-    <div class="row columns text-center">
-        <input type="Password" name="password-login" placeholder="Password">
+    <div class="form-group text-center">
+        <input class="form-control" type="Password" name="password-login" placeholder="Password">
     </div>
     <?php
         if(isset($Msg['password'])){echo "<h4 style='color:red;'>".$Msg['password']."</h4>";}
     ?>
     <br>
-    <input type="submit" name="submit-login" class="primary button expanded search-button" value="Login">
-    <h4>You do not have an account? </h4><a href="?register"><h4>Create acount</h4></a>
- </form>
+    <input type="submit" name="submit-login" class="btn btn-primary btn-lg btn-block" value="Login">
+    <br><br>
+    <h4 class="text-center">You do not have an account? </h4><a class="text-center" href="?register"><h4>Create acount</h4></a>
+    </div>
+    </div>
+  </form>
 </div>
 </div>
--- Ljetni_zadatak/bootstrap/Public/register.php
+++ Ljetni_zadatak/bootstrap/Public/register.php
@@ -10,41 +10,46 @@ if(isset($_POST['submit'])){
 <div class="login">
     <div class="translucent-form-overlay text-center centered">
     <form action="" method="POST">
+    <div class="form-group row">
+    <div class="col-sm-5">
+    <div class="form-group">
         <h3>Create account</h3>
         <br>
         <?php
         if(isset($errors['name'])){echo "<h4 style='color:red;'>".$errors['name']."</h4>";}
         ?>
-        <div class="row columns text-center">
-            <input type="text" name="Firstname" placeholder="Firstname">
+        <div class="form-group text-center">
+            <input class="form-control"  type="text" name="Firstname" placeholder="Firstname">
         </div>
         <?php
         if(isset($errors['lastname'])){echo "<h4 style='color:red;'>".$errors['lastname']."</h4>";}
         ?>
-        <div class="row columns text-center">
-            <input type="text" name="Lastname" placeholder="Lastname">
+        <div class="form-group text-center">
+            <input class="form-control"  type="text" name="Lastname" placeholder="Lastname">
         </div>
         <?php
         if(isset($errors['email'])){echo "<h4 style='color:red;'>".$errors['email']."</h4>";}
         ?>
-        <div class="row columns text-center">
-            <input type="text" name="Email" placeholder="Email">
+        <div class="form-group text-center">
+            <input class="form-control"  type="text" name="Email" placeholder="Email">
         </div>
         <?php
         if(isset($errors['password'])){echo "<h4 style='color:red;'>".$errors['password']."</h4>";}
         ?>
-        <div class="row columns text-center">
-            <input type="Password" name="Password" placeholder="Password">
+        <div class="form-group text-center">
+            <input class="form-control"  type="Password" name="Password" placeholder="Password">
         </div>
-        <div class="row columns text-center">
-            <input type="Password" name="Confirm-Password" placeholder="Confirm password">
+        <div class="form-group text-center">
+            <input class="form-control"  type="Password" name="Confirm-Password" placeholder="Confirm password">
         </div>
         <br>
-        <input type="submit" name="submit" class="primary button expanded search-button" value="Register">
+        <input type="submit" name="submit" class="btn btn-primary btn-lg btn-block" value="Register">
         <?php
         if(isset($errors['complete'])){echo "<h4 style='color:green;'>".$errors['complete']."</h4>";}
-        ?>
+        ?><br>
         <h4><a href="?login">Login</a></h4>
+        </div>
+    </div>
     </form>
     </div>
 </div>
