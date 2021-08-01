8ab082d49c114c8fcf72bbb4808674dc6b868c36
Ante Filipović <Ante.filipovic72@gmail.com>
2021-08-01 03:45:41 +0200

    finishing update product

--- Ljetni_zadatak/bootstrap/Public/Admin/update_product.php
+++ Ljetni_zadatak/bootstrap/Public/Admin/update_product.php
@@ -10,47 +10,49 @@ if(isset($_GET['productChange'])){
 ?>
 
 <form action="" method="post" enctype="multipart/form-data">
-  <div class="grid-container">
-    <div class="grid-x grid-padding-x">
-      <div class="cell small-12 large-8 large-offset-2">
+<div class="form-group row">
+    <div class="col-sm-5">
+      <div class="form-group text-center">
         <label><h4>TITLE</h4>
-          <input type="text" name="title" value="<?php echo $result[0]['title']?>">
+          <input class="form-control" type="text" name="title" value="<?php echo $result[0]['title']?>">
         </label>
       </div>
-      <div class="cell small-12 large-8 large-offset-2">
+      <div class="form-group text-center">
         <label><h4>AUTHOR</h4>
-          <input type="text" name="author" value="<?php echo $result[0]['author']?>">
+          <input class="form-control" type="text" name="author" value="<?php echo $result[0]['author']?>">
         </label>
       </div>
-      <div class="cell small-12 large-8 large-offset-2">
+      <div class="form-group text-center">
         <label><h4>IMAGE</h4>
-            <input type="file" name="image">
+            <input class="form-control" type="file" name="image">
             <img style="max-width:200px" src="images/<?php echo $result[0]['image'];?>">
         </label>
       </div>
-      <div class="cell small-12 large-8 large-offset-2">
+      <div class="form-group text-center">
         <label><h4>PRICE</h4>
-          <input type="text" name="price" value="<?php echo $result[0]['price']?>">
+          <input class="form-control" type="text" name="price" value="<?php echo $result[0]['price']?>">
         </label>
       </div>
-      <div class="cell small-12 large-8 large-offset-2">
+      <div class="form-group text-center">
         <label><h4>CATEGORY</h4>
-          <input type="text" name="category" value="<?php echo $result[0]['category']?>"">
+          <input class="form-control" type="text" name="category" value="<?php echo $result[0]['category']?>"">
         </label>
       </div>
-      <div class="cell small-12 large-8 large-offset-2">
+      <div class="form-group text-center">
         <label><h4>QUANTITY(int)</h4>
-          <input type="number" name="quantity" value="<?php echo $result[0]['quantity']?>">
+          <input class="form-control" type="number" name="quantity" value="<?php echo $result[0]['quantity']?>">
         </label>
       </div>
-      <div class="cell small-12 large-8 large-offset-2">
+      <div class="form-group text-center">
         <label><h4>CONTENT</h4>
-        <textarea id="w3review" name="content" rows="4" cols="50"><?php echo $result[0]['content']?></textarea>
+        <textarea class="form-control" id="w3review" name="content" rows="4" cols="50"><?php echo $result[0]['content']?></textarea>
         </label>
       </div>
-      <div class="cell small-12 large-8 large-offset-2">
-      <input type="submit" class="button" value="Update product" name="submit">
-      <a href="index.php?admin" class="alert button">Back</a>
+      <div class="form-group text-center">
+      <input type="submit" class="btn btn-primary" value="Update product" name="submit">
+      <a href="index.php?admin" class="btn btn-outline-danger">Back</a>
+      </div>
+      </div>
       </div>
     </div>
   </div>
