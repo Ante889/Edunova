bae99a82cbe137d80ebefd0e1cc3a5120bb18ee9
Ante Filipović <Ante.filipovic72@gmail.com>
2021-08-03 01:02:00 +0200

    -

--- Ljetni_zadatak/WithoutFramework/Public/configuration.php
+++
@@ -1,9 +0,0 @@
-<?php 
-ob_start();
-session_start();
-include "resources/database.php";
-include "resources/function.php";
-include "resources/productClass.php";
-
-define("TITLE", "MyShop");
-define("PATH", "http://polaznik07.edunova.hr/");
--- Ljetni_zadatak/WithoutFramework/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-ob_start();
-header("Location: Public/index.php");
-?>
