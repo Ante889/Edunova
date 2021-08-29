c48d917b169dca78cefa9794916f7abbfa1a9cd4
Ante Filipović <Ante.filipovic72@gmail.com>
2021-08-29 23:30:41 +0200

    test

--- Php_exercising/Homework-Login/content.php
+++ Php_exercising/Homework-Login/content.php
@@ -20,57 +20,27 @@ IsLogout('index.php');
     </div>
     <!-- End Top Bar -->
 
+<?php
 
-    <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
-      <ul class="orbit-container">
-        <button class="orbit-previous" aria-label="previous"><span class="show-for-sr">Previous Slide</span>&#9664;</button>
-        <button class="orbit-next" aria-label="next"><span class="show-for-sr">Next Slide</span>&#9654;</button>
-        <li class="orbit-slide is-active">
-          <img src="https://placehold.it/2000x750">
-        </li>
-        <li class="orbit-slide">
-          <img src="https://placehold.it/2000x750">
-        </li>
-        <li class="orbit-slide">
-          <img src="https://placehold.it/2000x750">
-        </li>
-        <li class="orbit-slide">
-          <img src="https://placehold.it/2000x750">
-        </li>
-      </ul>
-    </div>
 
+
+
+?>
     <div class="row column text-center">
       <h2><?php echo "Welcome ". $_SESSION['email'];?></h2>
       <hr>
     </div>
 
-    <div class="row small-up-2 large-up-4">
-      <div class="column">
-        <img class="thumbnail" src="https://placehold.it/300x400">
-        <h5>Nulla At Nulla Justo, Eget</h5>
-        <p>$400</p>
-        <a href="#" class="button expanded">Buy</a>
-      </div>
-      <div class="column">
-        <img class="thumbnail" src="https://placehold.it/300x400">
-        <h5>Nulla At Nulla Justo, Eget</h5>
-        <p>$400</p>
-        <a href="#" class="button expanded">Buy</a>
-      </div>
-      <div class="column">
-        <img class="thumbnail" src="https://placehold.it/300x400">
-        <h5>Nulla At Nulla Justo, Eget</h5>
-        <p>$400</p>
-        <a href="#" class="button expanded">Buy</a>
-      </div>
-      <div class="column">
-        <img class="thumbnail" src="https://placehold.it/300x400">
-        <h5>Nulla At Nulla Justo, Eget</h5>
-        <p>$400</p>
-        <a href="#" class="button expanded">Buy</a>
-      </div>
+    <div class="row column text-center">
+      <h2>Chat</h2>
+      <hr>
+      <h3>autor : </h3>
+      <h3>Poruka : </h3>
+      <hr>
+      <hr>
     </div>
+
+
 
     <hr>
 
@@ -87,4 +57,24 @@ include "footer.php";
 
 
 
+
+<?php  
+
+  function test(){
+    echo "test";
+  }
+?>
+
+<script>
+
+
+(function loop() {
+  setTimeout(function () {
+    document.write(' <?php test(); ?> ');
+    loop()
+  }, 1000);
+}());
+
+
+  </script>
 
