cd1b4815136b92f1a54594fb59d491cfdb70470e
Ante Filipović <Ante.filipovic72@gmail.com>
2021-09-01 23:22:51 +0200

    edunova 4

--- POLAZNIKPP23/app/konfiguracija.php
+++ POLAZNIKPP23/app/konfiguracija.php
@@ -2,5 +2,11 @@
 
 return [
     'nazivApp'=>'Edunova APP',
-    'url'=>'http://edunovapp23.xyz/'
+    'url'=>'http://edunovapp23.xyz/',
+    'baza'=>[
+        'server'=>'localhost',
+        'baza'=>'edunovapp23',
+        'korisnik'=>'edunova',
+        'lozinka'=>'edunova'
+    ]
 ];
